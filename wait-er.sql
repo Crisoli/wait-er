a8572c25c24597101dbccb7d2dacf9c8cc5715d4
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-07-29 12:19:51 -0300

    Add files via upload
    Implementação das tabelas requests

--- wait-er.sql
+++ wait-er.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 29-Jul-2019 às 12:31
+-- Generation Time: 29-Jul-2019 às 15:19
 -- Versão do servidor: 5.6.34
 -- PHP Version: 5.6.32
 
@@ -95,6 +95,14 @@ CREATE TABLE `requests` (
   `request_number` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `requests`
+--
+
+INSERT INTO `requests` (`id`, `id_foodmenu`, `quantity`, `request_number`) VALUES
+(9, 999, 1, 1),
+(10, 999, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -106,6 +114,48 @@ CREATE TABLE `requests_numbers` (
   `total` decimal(10,2) NOT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `requests_numbers`
+--
+
+INSERT INTO `requests_numbers` (`id`, `total`, `status`) VALUES
+(1, '5.00', 'Pendente'),
+(2, '5.00', 'Pendente'),
+(3, '5.00', 'Pendente'),
+(4, '5.00', 'Pendente'),
+(5, '5.00', 'Pendente'),
+(6, '5.00', 'Pendente'),
+(7, '5.00', 'Pendente'),
+(8, '5.00', 'Pendente'),
+(9, '5.00', 'Pendente'),
+(10, '5.00', 'Pendente'),
+(11, '5.00', 'Pendente'),
+(12, '5.00', 'Pendente'),
+(13, '5.00', 'Pendente'),
+(14, '5.00', 'Pendente'),
+(15, '5.00', 'Pendente'),
+(16, '5.00', 'Pendente'),
+(17, '5.00', 'Pendente'),
+(18, '3.50', 'Pendente'),
+(19, '3.50', 'Pendente'),
+(20, '3.50', 'Pendente'),
+(21, '3.50', 'Pendente'),
+(22, '3.50', 'Pendente'),
+(23, '3.50', 'Pendente'),
+(24, '3.50', 'Pendente'),
+(25, '3.50', 'Pendente'),
+(26, '5.00', 'Pendente'),
+(27, '5.00', 'Pendente'),
+(28, '5.00', 'Pendente'),
+(29, '5.00', 'Pendente'),
+(30, '1.50', 'Pendente'),
+(31, '1.50', 'Pendente'),
+(32, '1.50', 'Pendente'),
+(33, '1.50', 'Pendente'),
+(34, '5.00', 'Pendente'),
+(35, '5.00', 'Pendente'),
+(36, '5.00', 'Pendente');
 
 --
 -- Indexes for dumped tables
@@ -161,19 +211,19 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `foodmenu`
 --
 ALTER TABLE `foodmenu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
 
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `requests_numbers`
 --
 ALTER TABLE `requests_numbers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
