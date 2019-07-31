8555dd9806e8dc5ccc49c4e820a4259ebc523600
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-07-31 08:27:36 -0300

    DB_Name update

--- inc/database.php
+++ inc/database.php
@@ -4,7 +4,7 @@ mysqli_report(MYSQLI_REPORT_STRICT);
 $HOST = 'localhost';
 $DB_USERNAME = 'root';
 $DB_PASS = 'usbw';
-$DB_NAME = 'wait-er';
+$DB_NAME = 'waiter';
 $mysqli = new mysqli($HOST, $DB_USERNAME, $DB_PASS,$DB_NAME);
 return $mysqli;
 if(mysqli_connect_errno()){
