d991032c101110e45822e58bc36feef65fa8ac59
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-06 09:37:49 -0300

    Mudança de diretorios

--- foodlist.php
+++ foodlist.php
@@ -1,8 +1,6 @@
 
-<?php require_once 'config.php'; ?>	
-<?php require_once 'inc/database.php'; ?>	
-<?php include(HEADER_TEMPLATE); ?>	
-<?php include('inc/employers/foodlist.php'); ?>
+<?php require_once 'config.php'; ?>
+<?php require_once 'inc/database.php'; ?>
+<?php include(HEADER_TEMPLATE); ?>
+<?php include('inc/employers/shoppingcart.php'); ?>
 <?php include(FOOTER_TEMPLATE); ?>
-
-
