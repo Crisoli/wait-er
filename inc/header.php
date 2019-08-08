15c763e8208faec691a4d237174678a12c45ea73
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-08-08 08:53:17 -0300

    add font

--- inc/header.php
+++ inc/header.php
@@ -3,6 +3,7 @@
 <html>
 
 <head>
+
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Painel E-Waiter - Login</title>
@@ -21,4 +22,5 @@
    <!-- Materialize CSS --> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <!-- Font-Awesome CSS --> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
      <!-- Bootstrap CSS --> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Oxygen&display=swap" rel="stylesheet">
 </head>
