63c048d46b6d09eac605275b59187a13b2f78f24
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-12 17:44:21 -0300

    Add files via upload

---
+++ inc/comandapage.php
@@ -0,0 +1,81 @@
+<?php
+$date = date('Y-m-d', time());
+$hour= date('h:i:s', time());
+session_start();
+//Echo pedidos pendentes
+$requestpen = $mysqli->query("SELECT * FROM requests_numbers WHERE status = 'Pendente'");
+if(mysqli_num_rows($requestpen) > 0)
+{
+     while($pen = mysqli_fetch_array($requestpen))
+     {
+?>
+<form method="post">
+<div>
+  <input type="hidden" value=<?php echo $pen['id'];?> name="id">
+  <?php echo 'Pendente'?>
+  <?php echo $pen['id'];?>
+  <?php echo $pen['status'];?>
+  <?php echo $pen['obs'];?>
+  <?php echo $pen['total'];?>
+  <?php echo $pen['id_table'];?>
+  <?php echo $pen['started'];?>
+  <input type="submit" name="update" value="Finalizar Pedido"></input>
+
+</div>
+
+</form>
+<?php
+if(isset($_POST['id'])){
+$finalizar = $mysqli->query("UPDATE requests_numbers SET status='Finalizado',finished='".$date."',finished_hour='".$hour."', finish_id='".$_SESSION['userid']."' WHERE id = '".$_POST['id']."'");
+header('location:comanda.php');
+    }
+}
+    }
+?>
+
+<?php
+//Echo pedidos finalizados hoje
+$requestrec = $mysqli->query("SELECT * FROM requests_numbers WHERE status = 'Finalizado' AND started = '".$date."'");
+if(mysqli_num_rows($requestrec) > 0)
+{
+     while($rec = mysqli_fetch_array($requestrec))
+     {
+?>
+
+<div>
+  <?php echo 'Finalizado Recentemente'?>
+  <?php echo $rec['id'];?>
+  <?php echo $rec['status'];?>
+  <?php echo $rec['obs'];?>
+  <?php echo $rec['total'];?>
+  <?php echo $rec['id_table'];?>
+  <?php echo $rec['started'];?>
+</div>
+
+<?php
+}
+    }
+?>
+
+<?php
+//Echo pedidos finalizados
+$requestfin = $mysqli->query("SELECT * FROM requests_numbers WHERE status = 'Finalizado'");
+if(mysqli_num_rows($requestfin) > 0)
+{
+     while($fin = mysqli_fetch_array($requestfin))
+     {
+?>
+<div>
+  <?php echo 'Finalizado'?>
+  <?php echo $fin['id'];?>
+  <?php echo $fin['status'];?>
+  <?php echo $fin['obs'];?>
+  <?php echo $fin['total'];?>
+  <?php echo $fin['id_table'];?>
+  <?php echo $fin['started'];?>
+</div>
+
+<?php
+}
+    }
+?>
