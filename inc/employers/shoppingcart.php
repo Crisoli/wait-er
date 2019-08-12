d6d6049c2ec8eb31a895cd19e2695e7c3b3f1559
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-08-12 20:29:00 -0300

    Add files via upload

--- inc/employers/shoppingcart.php
+++ inc/employers/shoppingcart.php
@@ -2,12 +2,13 @@
  include 'inc/sidemenu.php';
 include 'inc/employers/shoppingcartarray.php';
 ?>
-     <body>
+     <body style="font-family: 'Exo 2', sans-serif;">
 
           <br/>
           <div class="container">
-                  <div class="row">
-                    <div class="col s12 m7 l4 offset-l4">
+             <div class="row">
+
+
                <?php
                $query = $mysqli->query("SELECT * FROM foodmenu ORDER BY id ASC");
                if(mysqli_num_rows($query) > 0)
@@ -16,53 +17,58 @@ include 'inc/employers/shoppingcartarray.php';
                     {
                ?>
 
+                   <div class="col s12 m3 l3">
+                  <div class="card midium">
+
+                           <div class="card-image">
+
+                            <form method="post" action="foodlist.php?action=add&id=<?php echo $row["id"]; ?>">
+                            <img src="<?php echo $row["image"]; ?>" class="responsive-img" alt=""/>
+
+                            </div>
+
+                            <div class="divider"></div>
+
+                            <span class="card-title center-align"> <?php echo $row["name"]; ?>
+                            <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
+                            </span>
+
+                            <div class="divider"></div>
+
+                            <div class="card-content center-align">
+
+                             <h5 class="">R$ <?php echo $row["price"]; ?></h5>
+
+                             <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+
+                            </div>
 
 
-
-                    <div class="card small">
-
-                  <div class="card-image">
-
-                    <form method="post" action="foodlist.php?action=add&id=<?php echo $row["id"]; ?>">
-
-
-
-                              <img src="<?php echo $row["image"]; ?>" class=" " alt=""/>
+                            <div class="card-action">
+                            <input type="submit" name="add_to_cart" style="background-color:#FF291F" class="btn btn-danger btn-flat white-text" value="+"/>
+                            <div class="col s2 m4 l2 ">
+                             <input type="text" name="quantity" class="form-control" value="1" />
 
                              </div>
-                             <div class="card-content">
+                             </div>
 
-                              <h4 class="text-info"><?php echo $row["name"]; ?></h4>
-                              <h4 class="text-danger">R$ <?php echo $row["price"]; ?></h4>
 
-                              <br><input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
-                              <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" /></br>
 
-                             <div class="card-action">
-                               <div class="col s2 m4 l2 float-right">
-                              <input type="text" name="quantity" class="form-control" value="1" />
 
-                                <div class="card-tittle">
-                                <input type="submit" name="add_to_cart" style="" class="waves-effect waves-teal btn-flat float-right" value="+"/>
-
-                                </div>
-</div>
-                               </div>
-                               </div>
- </div>
-
-                                 <!---  fim da parte de produtos -->
+                    </div>
+                     </div>
 
                     </form>
-               </div>
+
                <?php
                     }
                     include 'inc/employers/shoppingcartinsert.php';
                }
                ?>
+
+               </div>
+           </div>
                     </table>
 
-          </div>
-          </div>
-          <br />
+
      </body>
