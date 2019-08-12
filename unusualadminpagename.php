fc96b006ac8214a9de2862a5be73f1256bfe3d5d
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-12 08:30:34 -0300

    Delete unusualadminpagename.php

--- unusualadminpagename.php
+++
@@ -1,6 +0,0 @@
-
-<?php require_once 'config.php'; ?>
-<?php require_once 'inc/database.php'; ?>
-<?php include(HEADER_TEMPLATE); ?>
-<?php include('inc/.php');?>
-<?php include(FOOTER_TEMPLATE); ?>
