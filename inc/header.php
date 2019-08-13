d02aeef19bfb4e6d7f770d1e96e03c22c417a0b5
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-13 09:36:50 -0300

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,8 @@
 <?php
 echo 'Current PHP version: ' . phpversion();
 date_default_timezone_set('America/Sao_Paulo');
+$date = date('Y-m-d', time());
+$hour= date('h:i:s', time());
 ?>
 <!DOCTYPE html>
 <html>
