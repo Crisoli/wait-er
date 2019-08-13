0450b43205caf9e4377622c99dcbb72da9e69fec
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-13 09:37:29 -0300

    Add files via upload

--- comanda.php
+++ comanda.php
@@ -2,5 +2,5 @@
 <?php require_once 'config.php'; ?>
 <?php require_once 'inc/database.php'; ?>
 <?php include(HEADER_TEMPLATE); ?>
-<?php include('inc/comandapage.php');?>
+<?php include('inc/pedidos.php');?>
 <?php include(FOOTER_TEMPLATE); ?>
