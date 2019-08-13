7b8aebe8af922345f846ac2f109e0f1c609e66a4
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-13 09:35:49 -0300

    Add files via upload

--- inc/employers/shoppingcartarray.php
+++ inc/employers/shoppingcartarray.php
@@ -1,58 +1,60 @@
+
 <?php
-echo 'Current PHP version: ' . phpversion();
+require_once 'config.php';
 ?>
-<?php require_once 'config.php'; ?>
-<?php require_once 'inc/database.php'; ?>
+<?php
+require_once 'inc/database.php';
+?>
 <?php
 
- session_start();
+session_start();
 
- if(isset($_POST["add_to_cart"]))
- {
-      if(isset($_SESSION["shopping_cart"]))
+if (isset($_POST["add_to_cart"]))
+  {
+    if (isset($_SESSION["shopping_cart"]))
       {
-           $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
-           if(!in_array($_GET["id"], $item_array_id))
-           {
-                $count = count($_SESSION["shopping_cart"]);
-                $item_array = array(
-                     'item_id'               =>     $_GET["id"],
-                     'item_name'               =>     $_POST["hidden_name"],
-                     'item_price'          =>     $_POST["hidden_price"],
-                     'item_quantity'          =>     $_POST["quantity"]
-                );
-                $_SESSION["shopping_cart"][$count] = $item_array;
-           }
-           else
-           {
-                echo '<script>alert("Item Already Added")</script>';
+        $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+        if (!in_array($_GET["id"], $item_array_id))
+          {
+            $count                             = count($_SESSION["shopping_cart"]);
+            $item_array                        = array(
+                'item_id' => $_GET["id"],
+                'item_name' => $_POST["hidden_name"],
+                'item_price' => $_POST["hidden_price"],
+                'item_quantity' => $_POST["quantity"]
+            );
+            $_SESSION["shopping_cart"][$count] = $item_array;
+          }
+        else
+          {
+            echo '<script>alert("Item Already Added")</script>';
+            echo '<script>window.location="foodlist.php"</script>';
+          }
+      }
+    else
+      {
+        $item_array                   = array(
+            'item_id' => $_GET["id"],
+            'item_name' => $_POST["hidden_name"],
+            'item_price' => $_POST["hidden_price"],
+            'item_quantity' => $_POST["quantity"]
+        );
+        $_SESSION["shopping_cart"][0] = $item_array;
+      }
+  }
+if (isset($_GET["action"]))
+  {
+    if ($_GET["action"] == "delete")
+      {
+        foreach ($_SESSION["shopping_cart"] as $keys => $values)
+          {
+            if ($values["item_id"] == $_GET["id"])
+              {
+                unset($_SESSION["shopping_cart"][$keys]);
                 echo '<script>window.location="foodlist.php"</script>';
-           }
-      }
-      else
-      {
-           $item_array = array(
-                'item_id'               =>     $_GET["id"],
-                'item_name'               =>     $_POST["hidden_name"],
-                'item_price'          =>     $_POST["hidden_price"],
-                'item_quantity'          =>     $_POST["quantity"]
-           );
-           $_SESSION["shopping_cart"][0] = $item_array;
-      }
- }
- if(isset($_GET["action"]))
- {
-      if($_GET["action"] == "delete")
-      {
-           foreach($_SESSION["shopping_cart"] as $keys => $values)
-           {
-                if($values["item_id"] == $_GET["id"])
-                {
-                     unset($_SESSION["shopping_cart"][$keys]);
-                     echo '<script>window.location="foodlist.php"</script>';
-                }
-           }
+              }
+          }
       }
 
- }
- ?>
+  }
+?>
--- inc/employers/shoppingcartinsert.php
+++ inc/employers/shoppingcartinsert.php
@@ -12,59 +12,77 @@
           </tr>
           <form method="post">
           <?php
-          if(!empty($_SESSION["shopping_cart"]))
-          {
-               $total = 0;
-               foreach($_SESSION["shopping_cart"] as $keys => $values)
-               {
-          ?>
-          <tr>
-               <td><?php echo $values["item_name"]; ?></td>
-               <td><?php echo $values["item_quantity"]; ?></td>
-               <td>$ <?php echo $values["item_price"]; ?></td>
-               <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>
-               <td><a href="foodlist.php?action=update&id=<?php echo $values["item_id"];?>"></a><p><a href="foodlist.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+if (!empty($_SESSION["shopping_cart"]))
+  {
+    $total = 0;
+    foreach ($_SESSION["shopping_cart"] as $keys => $values)
+      {
+?>
+        <tr>
+               <td><?php
+        echo $values["item_name"];
+?></td>
+               <td><?php
+        echo $values["item_quantity"];
+?></td>
+               <td>$ <?php
+        echo $values["item_price"];
+?></td>
+               <td>$ <?php
+        echo number_format($values["item_quantity"] * $values["item_price"], 2);
+?></td>
+               <td><a href="foodlist.php?action=update&id=<?php
+        echo $values["item_id"];
+?>"></a><p><a href="foodlist.php?action=delete&id=<?php
+        echo $values["item_id"];
+?>"><span class="text-danger">Remove</span></a></td>
           </tr>
           <?php
-                    $total = $total + ($values["item_quantity"] * $values["item_price"]);
-               }
-          ?>
-          <tr>
+        $total = $total + ($values["item_quantity"] * $values["item_price"]);
+      }
+?>
+        <tr>
                <td colspan="3" align="right">Total</td>
-               <td align="right">$ <?php echo number_format($total, 2); ?></td>
+               <td align="right">$ <?php
+    echo number_format($total, 2);
+?></td>
                <td></td>
           </tr>
 
           <?php
-          if (isset($_POST['submit'])) {
-            $userid = $_SESSION['userid'];
-            $obs = $_POST['obs'];
-            $table = $_POST['table'];
-            $request_create = $mysqli->query("INSERT INTO requests_numbers VALUES (null, '".$total."','Pendente','".$obs."','".$userid."','".$table."',null,null)");
-            $request = $mysqli->query("SELECT id FROM requests_numbers ORDER BY id DESC LIMIT 1");
-            while($rowid = mysqli_fetch_array($request))
-            {
+    if (isset($_POST['submit']))
+      {
+        $userid         = $_SESSION['userid'];
+        $obs            = $_POST['obs'];
+        $table          = $_POST['table'];
+        $request_create = $mysqli->query("INSERT INTO requests_numbers VALUES (null, '" . $total . "','Pendente','" . $obs . "','" . $table . "','" . $date . "','" . $hour . "','" . $userid . "',null,null,null)");
+        $request        = $mysqli->query("SELECT id FROM requests_numbers ORDER BY id DESC LIMIT 1");
+        while ($rowid = mysqli_fetch_array($request))
+          {
             $lastid = $rowid['id'];
-            }
-          foreach ($_SESSION["shopping_cart"] as $key => $values) {
-            $item_id = $values['item_id'];
-            $item_quantity = $values['item_quantity'];
-            $shopping_cart_insert = $mysqli->query("INSERT INTO requests VALUES (null, '".$item_id."', '".$item_quantity."','".$lastid."')");
-            }
+            unset($_SESSION["shopping_cart"][$keys]);
+          }
+        foreach ($_SESSION["shopping_cart"] as $key => $values)
+          {
+            $item_id              = $values['item_id'];
+            $item_quantity        = $values['item_quantity'];
+            $shopping_cart_insert = $mysqli->query("INSERT INTO requests VALUES (null, '" . $item_id . "', '" . $item_quantity . "','" . $lastid . "')");
+          }
 
-          }
-          }
-          ?>
-          <select placeholder='Numero da Mesa' name='table' class="browser-default">
+      }
+  }
+?>
+        <select placeholder='Numero da Mesa' name='table' class="browser-default">
 
           <?php
-          $tablesreq = $mysqli->query("SELECT * FROM tables");
-          while($rowtable = mysqli_fetch_array($tablesreq)){
-            echo utf8_encode('<option value='.$rowtable['table_number'].'>'.$rowtable['table_number'].' - '.$rowtable['size'].' - '.$rowtable['status'].'</option>');
+$tablesreq = $mysqli->query("SELECT * FROM tables");
+while ($rowtable = mysqli_fetch_array($tablesreq))
+  {
+    echo utf8_encode('<option value=' . $rowtable['table_number'] . '>' . $rowtable['table_number'] . ' - ' . $rowtable['size'] . ' - ' . $rowtable['status'] . '</option>');
 
-          }
-          ?>
-        </select>
+  }
+?>
+      </select>
           <input type="text" name="obs" placeholder="Observações"> </input>
           <input type="submit" name="submit"></input>
         </form>
