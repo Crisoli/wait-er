0917986bc63ad1e0b639d34ff9288181f85240f4
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-08-19 12:15:14 -0300

    Add files via upload

--- inc/page.php
+++ inc/page.php
@@ -7,9 +7,7 @@ if(!isset($_SESSION['admin'])){
       <div class='row'>
           <div class='col-12'></div>
       </div>
-      <div class='row'>
-          <div class='col-12'></div>
-      </div>
+
 
       <div class='row'>
           <div class='offset-1'>
@@ -17,7 +15,7 @@ if(!isset($_SESSION['admin'])){
               <div class=''>
                   <div class='row container valign-wrapper'>
 
-                      <img src='inc/img/Waiter.png' class='w-100 p-100'>
+                      <img src='inc/img/Waiter.png' class='w-100 p-100 show-on-large'>
                   </div>
                   <div class='container'>
                       <div class='row'>
@@ -44,12 +42,12 @@ if(!isset($_SESSION['admin'])){
               </div>
           </div>
 
-          <div class='col s12 l5 m2 d-none d-sm-block'>
+          <div class='col s12 l5 m2 d-none d-xl-block show-on-large'>
                 <div class='container'>
               <div class='row container valign-wrapper'>
 
 
-                    <img src='inc/img/teste2.jpeg' class=''>
+                    <img src='inc/img/teste2.jpeg' class=' w-100 p-100'>
                   </div>
               </div>
           </div>
