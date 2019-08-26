46186a8bdc814b025f20188358125bfb920f971d
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-26 09:31:53 -0300

    Add files via upload

--- comanda.php
+++ comanda.php
@@ -2,5 +2,18 @@
 <?php require_once 'config.php'; ?>
 <?php require_once 'inc/database.php'; ?>
 <?php include(HEADER_TEMPLATE); ?>
-<?php include('inc/pedidos.php');?>
+<?php 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+     }
+if($_SESSION['pagead']=='1'){
+    include('inc/pedidos.php');
+}
+elseif($_SESSION['pagead']=='2'){
+    include('inc/cardapiocad.php');
+}
+elseif($_SESSION['pagead']=='3'){
+    echo 'page3';
+}     
+?>
 <?php include(FOOTER_TEMPLATE); ?>
