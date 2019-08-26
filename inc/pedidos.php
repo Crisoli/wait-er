c7824dcc781db7362b9e7ce5b6bbada2bf3b4176
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-08-26 12:24:36 -0300

    Add files via upload

--- inc/pedidos.php
+++ inc/pedidos.php
@@ -1,3 +1,4 @@
+
 <?php
 
 session_start();
@@ -8,21 +9,148 @@ if(mysqli_num_rows($requestpen) > 0)
      while($pen = mysqli_fetch_array($requestpen))
      {
 ?>
-<form method="post">
-<div>
-  <input type="hidden" value=<?php echo $pen['id'];?> name="id">
- 
-  <?php echo $pen['id'];?>
-  <?php echo $pen['status'];?>
-  <?php echo $pen['obs'];?>
-  <?php echo $pen['total'];?>
-  <?php echo $pen['id_table'];?>
-  <?php echo $pen['started'];?>
-  <input type="submit" name="update" value="Finalizar Pedido"></input>
 
+<div class="row">
+  <div class="container">
+</div>
+</div>
+<div class="row">
+  <div class="container">
+</div>
+</div>
+<div class="row">
+  <div class="container">
+</div>
 </div>
 
+
+<form method="post">
+  <div class="row">
+    <div class="container">
+    <div class="col s12 m6 l5">
+   <div class="white-grey">
+
+
+ <table>
+
+                <!--- Mesa, n° do pedido -->
+
+   <thead>
+           <tr style="background-color:black;" class="white-text">
+             <th >N° do pedido</th>
+              <th></th>
+             <th>Mesa</th>
+
+           </tr>
+   </thead>
+
+   <tbody>
+           <tr>
+               <td>
+                     <div ><?php echo $pen['id'];?></div>
+               </td>
+                <td></td>
+               <td>
+                     <?php echo $pen['id_table'];?>
+               </td>
+           </tr>
+  </tbody>
+
+
+                 <!--- Anotações -->
+    <thead>
+          <tr style="background-color:#E8EAEF">
+              <th>
+              </th>
+
+              <th class="center-align">Anotações</th>
+
+              <th>
+              </th>
+          </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+              <td>
+              </td>
+              <td>
+                <div class=""><?php echo $pen['obs'];?></div>
+              </td>
+              <td>
+              </td>
+        </tr>
+   </tbody>
+
+                   <!--- itens e preços -->
+    <thead style="background-color:#E8EAEF">
+
+            <th>Itens</th>
+            <th>Valor</th>
+            <th>Quantidade</th>
+
+
+   </thead>
+  <tbody>
+    <input type="hidden" value=<?php echo $pen['id'];?> name="id">
+
+
+
+   <tr>
+             <td>
+                <!--- nome da comida -->
+                  Pão
+             </td>
+
+             <td>
+                <div class=""><?php echo $pen['total'];?></div>
+             </td>
+
+             <td>
+               <!--- quantidade de comida -->
+               3
+             </td>
+  </tr>
+
+  <thead style="background-color:#E8EAEF">
+          <tr>
+            <th>Data</th>
+            <th></th>
+            <th>Status</th>
+          </tr>
+  </thead>
+
+  <tbody>
+          <tr>
+              <td>
+                    <?php echo $pen['started'];?>
+              </td>
+               <td></td>
+              <td>
+                    <?php echo $pen['status'];?>
+              </td>
+          </tr>
+ </tbody>
+
+
+
+
+  </div>
+
+        </tbody>
+
+      </table>
+
+
+       <br><input style="width:100%;" type="submit" name="update" value=">"></input></br>
 </form>
+
+</div>
+</div>
+</div>
+</div>
+
+
 <?php
 if(isset($_POST['id'])){
 $finalizar = $mysqli->query("UPDATE requests_numbers SET status='Finalizado',finished='".$date."',finished_hour='".$hour."', finish_id='".$_SESSION['userid']."' WHERE id = '".$_POST['id']."'");
@@ -76,3 +204,8 @@ if(mysqli_num_rows($requestfin) > 0)
 }
     }
 ?>
+<script type="text/javascript">
+ $(document).ready(function(){
+   $('.collapsible').collapsible();
+ });
+</script>
