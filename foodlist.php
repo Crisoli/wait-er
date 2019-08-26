9d7d0b87673e58bb4af79552bd571bbe5300e59a
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-26 11:57:04 -0300

    Add files via upload

--- foodlist.php
+++ foodlist.php
@@ -1,6 +1,9 @@
 
 <?php require_once 'config.php'; ?>
 <?php require_once 'inc/database.php'; ?>
+<?php     include 'inc/sidemenu.php'; ?>
 <?php include(HEADER_TEMPLATE); ?>
 <?php include('inc/employers/shoppingcart.php'); ?>
 <?php include(FOOTER_TEMPLATE); ?>
+
+
