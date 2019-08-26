c1b2c44b83084419f8caa864eb3f6c0cda4df3ec
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-08-26 09:32:52 -0300

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,10 @@
 <?php
-
+echo 'Current PHP version: ' . phpversion();
 date_default_timezone_set('America/Sao_Paulo');
 $date = date('Y-m-d', time());
 $hour= date('h:i:s', time());
 ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -16,16 +17,10 @@ $hour= date('h:i:s', time());
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="style.css" />
 
-
     <!-- Materialize JS --> <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
             <!-- JQuery --> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
          <!-- Popper JS --> <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <!-- Bootstrap JS --> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <!-- Simple Cart JS --> <script src="js/jQuery.SimpleCart.js" ></script>
-       <!-- Simple Cart --> <link href="css/simple_cart.css" rel="stylesheet">
       <!-- Google Icons --> <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
    <!-- Materialize CSS --> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <!-- Font-Awesome CSS --> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-     <!-- Bootstrap CSS --> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Oxygen&display=swap" rel="stylesheet">
+
 </head>
