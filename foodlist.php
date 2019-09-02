8e4343ee739d32c9079258eff52bdd22d14f40d6
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-09-02 12:18:47 -0300

    Add files via upload

--- foodlist.php
+++ foodlist.php
@@ -3,7 +3,25 @@
 <?php require_once 'inc/database.php'; ?>
 <?php     include 'inc/sidemenu.php'; ?>
 <?php include(HEADER_TEMPLATE); ?>
-<?php include('inc/employers/shoppingcart.php'); ?>
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+     }
+     
+     if(isset($_SESSION['admin'])){
+         if(!isset($_SESSION['pagefu'])){
+             $_SESSION['pagefu']='1';
+         }
+if($_SESSION['pagefu']=='1'){
+    include('inc/employers/shoppingcart.php');
+}
+elseif($_SESSION['pagefu']=='2'){
+    include('inc/employers/ranking.php');
+}
+
+     }
+
+?>
 <?php include(FOOTER_TEMPLATE); ?>
 
 
