32d9e9f9e84d86881edaa9d8986a40d28a90e8f4
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-09-09 09:39:18 -0300

    Add files via upload

--- inc/employers/shoppingcart.php
+++ inc/employers/shoppingcart.php
@@ -6,7 +6,7 @@ include 'inc/employers/shoppingcartarray.php';
 
           <br/>
             <div class="row">
-              <div class="col s7 m7 l7">
+
 
                <?php
                $query = $mysqli->query("SELECT * FROM foodmenu ORDER BY id ASC");
@@ -18,30 +18,30 @@ include 'inc/employers/shoppingcartarray.php';
 
 
 
-
-                            <form method="post" action="foodlist.php?action=add&id=<?php echo $row["id"]; ?>">
+                              <div class="col s12 m7 l4 offset-l1">
+                               <form method="post" action="foodlist.php?action=add&id=<?php echo $row["id"]; ?>">
 
 
                                     <div class="card">
-                                      <div class="card-image">
-                                        <img src="<?php echo $row["image"]; ?>" />
-                                        <span class="card-title black-text"><?php echo $row["name"]; ?></span>
-                                        <input type="hidden" name="hidden_name"  value="<?php echo $row["name"]; ?>" />
-                                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+                                          <div class="card-image">
+                                            <img src="<?php echo $row["image"]; ?>" />
+                                            <span class="card-title black-text"><?php echo $row["name"]; ?></span>
+                                            <input type="hidden" name="hidden_name"  value="<?php echo $row["name"]; ?>" />
+                                            <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
 
-                                        <input type='submit' class="btn-floating halfway-fab waves-light red darken-1" ; name='add_to_cart' style="border:none;" value="+"></input>
-                                      </div>
-                                      <input type="number" name="quantity" class="form-control" value="1" style="border-bottom: 2px solid black; background-color:; width:40%; position: absolute; right: 0px;"/>
-                                      <div class="card-content">
-                                        <h5 class="">R$ <?php echo $row["price"]; ?></h5>
-                                        <p><?php echo $row['promodesc'];?></p>
-                                      </div>
+                                            <input type='submit' class="btn-floating halfway-fab waves-light red darken-1" ; name='add_to_cart' style="border:none;" value="+"></input>
+                                          </div>
+                                          <input type="number" name="quantity" class="form-control" value="1" style="border-bottom: 2px solid black; background-color:; width:40%; position: absolute; right: 0px;"/>
+                                          <div class="card-content">
+                                            <h5 class="">R$ <?php echo $row["price"]; ?></h5>
+                                            <p><?php echo $row['promodesc'];?></p>
+                                          </div>
                                     </div>
 
 
 
 
-
+                                      </div>
 
                     </form>
                <?php
@@ -51,11 +51,9 @@ include 'inc/employers/shoppingcartarray.php';
 
 
                ?>
-             </div>
 
-               <div class="col s5 m5 l5">
-                 AAAAAAAAAAA
-               </div>
+
+
                </div>
 
 
