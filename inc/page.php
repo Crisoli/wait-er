940a14ddf1ac07cb0fbbf059bf92babd8aec3233
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-09-09 08:12:06 -0300

    Add files via upload

--- inc/page.php
+++ inc/page.php
@@ -25,9 +25,16 @@ else{
 ?>
 <body>
  <style>
- .wallpa{
-    background-image:url'(inc/img/wallpa.png)'
+#wallpa{
+    background-image:url(img/wallpa.png);
  }
+ .card{
+  column-break-inside: avoid;
+  .card {
+    display: inline-block;
+    overflow: visible;
+  }
+}
 </style>
 <div class="wallpa">
     <div class="row">
@@ -49,29 +56,56 @@ else{
                 <div class="row">
                     <div class="col-12"></div>
                 </div>
+                <div class="row">
+                    <div class="col-12"></div>
+                </div>
+
 
 
     <div class="row">
-        <div class="col s10 m4 l4 offset-s1 offset-m4 offset-l4">
-            <div class="card-panel z-depth-1">
-                    <form method="post">
 
-                    <div class="center-align">
-                      <img src='inc/img/waiterlogo.png' class='' style='
-                      width:50%;
-                      position: relative;
-                      top: -80px;
+                  <div class="col s12 m4 l4 offset-l2">
+                  <div class="card-panel z-depth-1" style=" position: relative; left: 22px; height:500px">
+                          <form method="post">
 
-                      '>
-                    </div>
-                            <p>
-                                <input type="text" name="username" placeholder="Nome" required>
-                            </p>
-                            <p>
-                                <input type="password" name="password" placeholder="Senha" id="password" class="" required>
-                            </p>
-                                  <input type='submit' class="" ; style="border:none;" value="Entrar"></input>
+                            <div class="row">
+                                <div class="col-12"></div>
+                            </div>
 
+                            <div class="row">
+                                <div class="col-12"></div>
+                            </div>
+                            <div class="hide-on-small-only">
+                            <div class="row">
+                                <div class="col-12"></div>
+                            </div>
+                            </div>
+
+                            <div class="center-align">
+                            <h3>Login</h3>
+                            </div>
+
+                                    <div class="col s12 m5 l10 offset-l1">
+                                          <div class="center-align">
+                                  <p>
+                                      <input type="text" name="username" placeholder="Nome" required>
+                                  </p>
+
+                                  <div class="row">
+                                      <div class="col-12"></div>
+                                  </div>
+                                  <div class="row">
+                                      <div class="col-12"></div>
+                                  </div>
+                                  <div class="row">
+                                      <div class="col-12"></div>
+                                  </div>
+
+                                  <p>
+                                      <input type="password" name="password" placeholder="Senha" id="password" class="" required>
+                                  </p>
+                                            </div>
+                                      </div>
 
                                   <div class="row">
                                       <div class="col-12"></div>
@@ -80,11 +114,44 @@ else{
                                       <div class="col-12"></div>
                                   </div>
 
-                    </form>
-            </div>
+                                        <div class="center-align">
+                                        <input type='submit' class="" ; style="border:none;
+                                        background-color:#44c767;
+                                        background-color:transparent;
+                                      	-moz-border-radius:17px;
+                                      	-webkit-border-radius:17px;
+                                      	border-radius:17px;
+                                      	border:3px solid black;
+                                      	display:inline-block;
+                                      	cursor:pointer;
+                                      	color:#ffffff;
+                                      	font-family:Arial;
+                                      	font-size:20px;
+                                        color: black;
+                                      	padding:15px 76px;
+                                      	text-decoration:none;
+                                        " value="Entrar"></input>
+
+                                </form>
+                                </div>
+                                 </div>
+                               </div>
+                                           <div class="hide-on-small-only">
+                                           <div class="col m4 l3" >
+                                           <div class="card-panel black"  style="height:500px">
+                                             <img src="inc/img/waiterlogo.png" style="height:284px; position: relative; left: -70px; top:85px;">
+                                           </div>
+                                           </div>
+                                           </div>
+
         </div>
-        </div>
-        </div>
+
+                                  <div class="row">
+                                      <div class="col-12"></div>
+                                  </div>
+
+
+      </div>
 
 
 
