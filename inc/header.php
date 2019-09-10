6a8bf9b30f52f1f511137ce6d8f41a4628955e7f
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-09-10 09:37:52 -0300

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -22,13 +22,14 @@ $hour= date('h:i:s', time());
    <!-- JQuery --> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
    <!-- Popper JS --> <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
    <!-- Google Icons --> <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   <link rel="stylesheet" href="https://unpkg.com/flexmasonry/dist/flexmasonry.css">
+   <script src="https://unpkg.com/flexmasonry/dist/flexmasonry.js"></script>
+
 
      <!-- Compiled and minified CSS -->
-     
+
 
 <!-- Compiled and minified JavaScript -->
-        
+
 
 </head>
-
-
