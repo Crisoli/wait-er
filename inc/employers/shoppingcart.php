41cd4da3f9fb768d1f5b08540ab43d3c68879d6a
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-09-10 09:37:10 -0300

    Add files via upload

--- inc/employers/shoppingcart.php
+++ inc/employers/shoppingcart.php
@@ -2,26 +2,35 @@
 include 'inc/employers/shoppingcartarray.php';
 
 ?>
+
+
+
      <body style="font-family: 'Exo 2', sans-serif;">
           <br/>
             <div class="row">
-
+            <div class='grid'>
 
                <?php
-               $query = $mysqli->query("SELECT * FROM foodmenu ORDER BY id ASC");
-               if(mysqli_num_rows($query) > 0)
-               {
+
+               $categorygrid = $mysqli->query("SELECT * FROM category");
+                    while($catgrid = mysqli_fetch_array($categorygrid))
+                    {?>
+                      <div id='
+                      <?php
+                    echo $catgrid['name'];
+                      ?>
+                      '>
+                      <?php
+                    }
+               $query = $mysqli->query("SELECT * FROM foodmenu ORDER BY id ASC WHERE category_id = $_SESSION['']");
                     while($row = mysqli_fetch_array($query))
                     {
                ?>
-
-                              <div class="col s12 m4 l3">
+                            <div id='<?php echo $catgrid['name']?>'>
                                <form method="post" action="foodlist.php?action=add&id=<?php echo $row["id"]; ?>">
-
-
-                                    <div class="card">
+                                      <div class="card">
                                           <div class="card-image">
-                                            <img src="<?php echo $row["image"]; ?>" style="height:200px;"/>
+                                            <img src="<?php echo $row["image"]; ?>" style="max-height:200px;"/>
                                             <span class="card-title black-text"><?php echo $row["name"]; ?></span>
                                             <input type="hidden" name="hidden_name"  value="<?php echo $row["name"]; ?>" />
                                             <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
@@ -34,24 +43,29 @@ include 'inc/employers/shoppingcartarray.php';
                                             <p><?php echo $row['promodesc'];?></p>
                                           </div>
                                     </div>
-
-
-
-
-                                      </div>
-
                     </form>
+                  </div>
                <?php
                     }
 
-               }
-
-
                ?>
-
-
-
+            </div>
                </div>
+             </div>
 
+               <script>
+
+               FlexMasonry.init('.grid',{
+                 breakpointCols: {
+        'min-width: 1500px': 6,
+        'min-width: 1200px': 5,
+        'min-width: 992px': 4,
+        'min-width: 768px': 3,
+        'min-width: 576px': 2,
+    },
+                 responsive: true,
+               });
+
+               </script>
 
      </body>
