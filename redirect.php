1abbdfef6b00fe80b8274113aa39608dcb0302b7
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-09-10 09:37:26 -0300

    Add files via upload

--- redirect.php
+++ redirect.php
@@ -1,6 +1,7 @@
 <?php
-session_start(); 
+session_start();
 $_SESSION['pagead']=$_GET['pagead'];
 $_SESSION['pagefu']=$_GET['pagefu'];
+$_SESSION['category']=$_GET['cat'];
 echo "<html><script>window.location.href = 'index.php'</script><html>";
 ?>
