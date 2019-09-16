a2da346f86370a53d52f1b553512fa91cffa1e07
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-09-16 12:11:43 -0300

    Add files via upload

--- inc/employers/shoppingcart.php
+++ inc/employers/shoppingcart.php
@@ -3,49 +3,38 @@ include 'inc/employers/shoppingcartarray.php';
 
 ?>
 
-
-
      <body style="font-family: 'Exo 2', sans-serif;">
           <br/>
-            <div class='grid'>
+            <div class='grid' id='grid-ajax'>
 
-         
-              <?php
-
-
-               $query = $mysqli->query("SELECT * FROM foodmenu WHERE category_id = '".$_SESSION['category']."'");
-                    while($row = mysqli_fetch_array($query))
-                    {
-               ?>
-                               <form method="post" action="foodlist.php?action=add&id=<?php echo $row["id"]; ?>">
-                                      <div class="card">
-                                          <div class="card-image">
-                                            <img src="<?php echo $row["image"]; ?>"/>
-                                            <span class="card-title black-text"><?php echo $row["name"]; ?></span>
-                                            <input type="hidden" name="hidden_name"  value="<?php echo $row["name"]; ?>" />
-                                            <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
-
-                                            <input type='submit' class="btn-floating halfway-fab waves-light red darken-1" ; name='add_to_cart' style="border:none;" value="+"></input>
-                                          </div>
-                                          <input type="number" name="quantity" class="form-control" value="1" style="border-bottom: 2px solid black; background-color:; width:40%; position: absolute; right: 0px;"/>
-                                          <div class="card-content">
-                                            <h5 class="">R$ <?php echo $row["price"]; ?></h5>
-                                            <p><?php echo $row['promodesc'];?></p>
-                                          </div>
-                                    </div>
-                    </form>
-               <?php
-                    }
-
-               ?>
             </div>
+
+            <button id="reveal" onclick="sendTheAJAX()" class="button">Why's that?</button>
 
                <script>
 
-               FlexMasonry.init('.grid',{
-                 responsive: true,
-               });
+
+
+               // 1. create a new XMLHttpRequest object -- an object like any other!
+              var myRequest = new XMLHttpRequest();
+              // 2. open the request and pass the HTTP method name and the resource as parameters
+              myRequest.open('GET', 'inc/employers/gridshow.php');
+              // 3. write a function that runs anytime the state of the AJAX request changes
+              myRequest.onreadystatechange = function () {
+             // 4. check if the request has a readyState of 4, which indicates the server has responded (complete)
+             if (myRequest.readyState === 4) {
+             // 5. insert the text sent by the server into the HTML of the 'ajax-content'
+             document.getElementById('grid-ajax').innerHTML = myRequest.responseText;
+                                             }
+                                                        };
+          $(document).ready(function sendTheAJAX(){
+                myRequest.send();
+                document.getElementById('reveal').style.display = 'none';
+                                  });
+
+                                  setInterval(function sendTheAJAX(){
+                           $('#grid-ajax').load('inc/employers/gridshow.php');
+                        }, 2000) /* time in milliseconds (ie 2 seconds)*/
 
                </script>
-
      </body>
