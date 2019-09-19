b6b15a52a47b3c7117c45f7f78931b8dc174c453
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-09-19 15:19:27 -0300

    Add files via upload

--- inc/employers/shoppingcartinsert.php
+++ inc/employers/shoppingcartinsert.php
@@ -1,44 +1,11 @@
 
           <form method="post">
-          <?php
-
-
+<?php
 $HOST = 'localhost';
 $DB_USERNAME = 'root';
 $DB_PASS = 'usbw';
 $DB_NAME = 'waiter';
 $mysqli = new mysqli($HOST, $DB_USERNAME, $DB_PASS,$DB_NAME);
-          
-if (!empty($_SESSION["shopping_cart"]))
-  {
-    $total = 0;
-    foreach ($_SESSION["shopping_cart"] as $keys => $values)
-      {
-?>
-
-               <?php
-        echo $values["item_name"];
-?>
-               <?php
-        echo $values["item_quantity"];
-?>
-               $ <?php
-        echo $values["item_price"];
-?>
-              $ <?php
-        echo number_format($values["item_quantity"] * $values["item_price"], 2);
-?>
-               </a><a href="foodlist.php?action=delete&id=<?php
-        echo $values["item_id"];
-?>"><span class="text-danger">Remove</span></a>
-
-          <?php
-        $total = $total + ($values["item_quantity"] * $values["item_price"]);
-      }
-?>
-
-               $ <?php
-    echo number_format($total, 2);
 ?>
 
           <?php
@@ -64,7 +31,7 @@ if (!empty($_SESSION["shopping_cart"]))
           }
 
       }
-  }
+  
 ?>
         <select placeholder='Numero da Mesa' name='table' class="browser-default">
 
