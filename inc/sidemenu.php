d11a10abcf61fc670261ae5605fedf17a4209bd7
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-09-19 08:45:22 -0300

    Add files via upload

--- inc/sidemenu.php
+++ inc/sidemenu.php
@@ -87,16 +87,42 @@ if($_SESSION['admin']=='false'){
   </ul>
 <?php
 if($_SESSION['admin']=='false'){
-echo "
+?>
 <div class='container' >
     <ul id='slide-out2' class='side-nav' style='background-color:white; width:95%;'>
-";
-          include 'inc/employers/shoppingcartinsert.php';
-echo "
-   </ul>
-</div>";
-}
 
+
+          <?php include 'inc/employers/shoppingcartinsert.php'; ?>
+
+              <script>
+
+               // 1. create a new XMLHttpRequest object -- an object like any other!
+              var myRequest = new XMLHttpRequest();
+              // 2. open the request and pass the HTTP method name and the resource as parameters
+              myRequest.open('GET', 'inc/employers/gridshow.php');
+              // 3. write a function that runs anytime the state of the AJAX request changes
+              myRequest.onreadystatechange = function () {
+             // 4. check if the request has a readyState of 4, which indicates the server has responded (complete)
+             if (myRequest.readyState === 4) {
+             // 5. insert the text sent by the server into the HTML of the 'ajax-content'
+             document.getElementById('slide-out2').innerHTML = myRequest.responseText;
+                                             }
+                                                        };
+          $(document).ready(function sendTheAJAX(){
+                myRequest.send();
+                document.getElementById('reveal').style.display = 'none';
+                                  });
+
+                                  setInterval(function sendTheAJAX(){
+                           $('#slide-out2').load('inc/employers/shoppingcartinsert.php');
+                        }, 2000) /* time in milliseconds (ie 2 seconds)*/
+
+               </script>
+
+   </ul>
+</div>
+<?php
+}
 ?>
 
 
