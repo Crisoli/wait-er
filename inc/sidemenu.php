108646fc073581045270802ffa9db816c07ab24a
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-09-23 08:16:32 -0300

    Add files via upload

--- inc/sidemenu.php
+++ inc/sidemenu.php
@@ -11,8 +11,9 @@
 <div id="page-container">
 <div id="content-wrap">
 
-  <nav id="navbar" class="nav-extended">
+  <nav id="navbar" class="nav-extended  sidenav-fixed">
     <div class="nav-wrapper">
+    <a href="#" class="brand-logo center">  <img src='inc/img/Waiterlogo.png' style="position:static; max-height:50%; max-width:40%; padding-bottom:1px;"> </a>
   <?php if($_SESSION['admin']=='false'){
     echo "
       <a href='#' data-activates='slide-out2' class='button-collapse show-on-medium-and-up right sidenav1 '><i class='material-icons'>menu</i></a>
@@ -21,12 +22,11 @@
   ?>
      <a href="#" data-activates="slide-out" class="button-collapse show-on-medium-and-up"><i class="material-icons">menu</i></a>
 
-      <a href="#" class="brand-logo center">  <img src='inc/img/Waiterlogo.png' style='max-height:200%; max-width:100px;'> </a>
-
 <?php
 if(isset($_SESSION['pagead'])){
   if($_SESSION['pagead']==1){
-    ?><div class='nav-content'>
+    ?>
+    <div class='nav-content'>
   <ul class='tabs tabs-transparent'>
     <li class='tab'><a href='#pen'>Pendentes</a></li>
     <li class='tab'><a href='#fin'>Finalizados</a></li>
@@ -58,7 +58,7 @@ $categorys = $mysqli->query("SELECT * FROM category");
 
         <ul id="slide-out" class="side-nav " style="background-color:black;">
         <div class="user-view">
-          <a href="#user"><img class="circle z-depth-2" style="max-height:200%; max-width:70px;" src="<?php echo $_SESSION['img'];?>"></a></br>
+          <a href="#user"><img class="circle z-depth-2" style="max-height:200%; max-width:70px; position:relative; padding-top:30px;" src="<?php echo $_SESSION['img'];?>"></a></br>
           <a href="#name"><span class="white-text"><?php echo $_SESSION['username'];?></span></a>
           <a href='logoff.php' name='logoff'><span class="white-text">Logoff</span></a>
         </div>
@@ -87,45 +87,17 @@ if($_SESSION['admin']=='false'){
   </ul>
 <?php
 if($_SESSION['admin']=='false'){
-?>
-<div class='container' >
+echo "
+<div class='container'>
     <ul id='slide-out2' class='side-nav' style='background-color:white; width:95%;'>
-
-
-          <?php include 'inc/employers/shoppingcartinsert.php'; ?>
-
-              <script>
-
-               // 1. create a new XMLHttpRequest object -- an object like any other!
-              var myRequest = new XMLHttpRequest();
-              // 2. open the request and pass the HTTP method name and the resource as parameters
-              myRequest.open('GET', 'inc/employers/gridshow.php');
-              // 3. write a function that runs anytime the state of the AJAX request changes
-              myRequest.onreadystatechange = function () {
-             // 4. check if the request has a readyState of 4, which indicates the server has responded (complete)
-             if (myRequest.readyState === 4) {
-             // 5. insert the text sent by the server into the HTML of the 'ajax-content'
-             document.getElementById('slide-out2').innerHTML = myRequest.responseText;
-                                             }
-                                                        };
-          $(document).ready(function sendTheAJAX(){
-                myRequest.send();
-                document.getElementById('reveal').style.display = 'none';
-                                  });
-
-                                  setInterval(function sendTheAJAX(){
-                           $('#slide-out2').load('inc/employers/shoppingcartinsert.php');
-                        }, 2000) /* time in milliseconds (ie 2 seconds)*/
-
-               </script>
-
+";
+          include 'inc/employers/shoppingcartinsert.php';
+echo "
    </ul>
-</div>
-<?php
+</div>";
 }
+
 ?>
-
-
 
 
 <script type="text/javascript">
