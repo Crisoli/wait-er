77277130e47da914268c7850a67f921d1680d02c
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-09-30 12:07:56 -0300

    Add files via upload

--- foodlist.php
+++ foodlist.php
@@ -7,7 +7,7 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
      }
-     
+
      if(isset($_SESSION['admin'])){
          if(!isset($_SESSION['pagefu'])){
              $_SESSION['pagefu']='1';
@@ -18,10 +18,11 @@ if($_SESSION['pagefu']=='1'){
 elseif($_SESSION['pagefu']=='2'){
     include('inc/employers/ranking.php');
 }
+elseif($_SESSION['pagefu']=='profile'){
+    include('inc/profile.php');
+}
 
      }
 
 ?>
 <?php include(FOOTER_TEMPLATE); ?>
-
-
