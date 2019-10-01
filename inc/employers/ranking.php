ab048d591c4e164a5bfc3a3fa69c725d4cc627db
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-10-01 09:26:46 -0300

    Add files via upload

--- inc/employers/ranking.php
+++ inc/employers/ranking.php
@@ -1,17 +1,24 @@
 
 
-     <body style="font-family: 'Exo 2', sans-serif;">
-<?php
-              $queryrank = $mysqli->query("SELECT COUNT(*) FROM requests_numbers GROUP BY starter_id ORDER BY COUNT(*) DESC; ");
-            while($row = mysqli_fetch_array($queryrank))
-            {
+     <body>
+      <div id='thegoddamnrankdiv_ajax'>
+       <script>
+       var myRequest = new XMLHttpRequest();
+       myRequest.open('GET', 'inc/employers/employers_select/rank_select.php');
+       myRequest.onreadystatechange = function () {
+       if (myRequest.readyState === 4) {
+       document.getElementById('thegoddamnrankdiv_ajax').innerHTML = myRequest.responseText;
+                                     }
+                                                };
+       $(document).ready(function side_show(){
+        myRequest.send();
+        document.getElementById('reveal').style.display = 'none';
+                          });
+                          setInterval(function side_show(){
+                   $('#thegoddamnrankdiv_ajax').load('inc/employers/employers_select/rank_select.php');
+                }, 2000) /* time in milliseconds (ie 2 seconds)*/
 
+       </script>
 
-                ?>
-
-
-                  <?php
-                  }
-                  ?>
 
      </body>
