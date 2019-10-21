02e21a9165dc860ed2e64cc3f9cbe2f2643c13c5
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-10-21 09:35:50 -0300

    Add files via upload

--- inc/employers/employers_select/side_select.php
+++ inc/employers/employers_select/side_select.php
@@ -7,7 +7,17 @@ $mysqli = new mysqli($HOST, $DB_USERNAME, $DB_PASS,$DB_NAME);
 
 session_start();
 ?>
+<div class="row">
+<div class="col s12 m12 l12">
+</div>
+</div>
+<div class="row">
+<div class="col s12 m12 l12">
+</div>
+</div>
 
+<div class="row">
+<div class="col s10 m10 l10 offset-l1 offset-s1 offset-m1">
 <?php
 if (!empty($_SESSION["shopping_cart"]))
   {
@@ -15,22 +25,20 @@ if (!empty($_SESSION["shopping_cart"]))
     foreach ($_SESSION["shopping_cart"] as $keys => $values)
       {
 ?>
-
+            <p><div class="col s9 m9 l9"> <?php
+        echo $values["item_quantity"];
+?>x
                <?php
         echo $values["item_name"];
-?>
-               <?php
-        echo $values["item_quantity"];
-?>
-               $ <?php
+?> </div>
+
+               <div class="offset-l4 offset-s1 offset-m1"> R$ <?php
         echo $values["item_price"];
-?>
-              $ <?php
-        echo number_format($values["item_quantity"] * $values["item_price"], 2);
-?>
+?> </div></p>
                <a href="foodlist.php?action=delete&id=<?php
         echo $values["item_id"];
-?>"><span class="text-danger">Remover pedido</span></a>
+?>"><p><div class="center-align"><span class="red-text">Remover pedido</span></div></p></a>
+<div class="divider"></div>
 
           <?php
         $total = $total + ($values["item_quantity"] * $values["item_price"]);
@@ -39,8 +47,10 @@ if (!empty($_SESSION["shopping_cart"]))
 
 ?>
 
-               $ <?php
+               <h1><div class="center-align"> R$<?php
     echo number_format($total, 2);
   }
 
-?>
+?></div></h1>
+</div>
+</div>
--- inc/employers/employers_select/table_select.php
+++ inc/employers/employers_select/table_select.php
@@ -5,7 +5,8 @@ $DB_PASS = 'usbw';
 $DB_NAME = 'waiter';
 $mysqli = new mysqli($HOST, $DB_USERNAME, $DB_PASS,$DB_NAME);
 ?>
-
+<div class="row">
+<div class="col s10 m10 l10 offset-l1 offset-s1">
 <select placeholder='Numero da Mesa' name='table' class="browser-default">
 <?php
 $tablesreq = $mysqli->query("SELECT * FROM tables where status ='Livre'");
@@ -15,3 +16,5 @@ echo utf8_encode('<option value=' . $rowtable['table_number'] . '>' . $rowtable[
 }
 ?>
 </select>
+</div>
+</div>
