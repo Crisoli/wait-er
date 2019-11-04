c30df9aad0e02eee8d993963739b323fdac9344c
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-11-04 12:12:13 -0300

    Add files via upload

--- delete.php
+++ delete.php
@@ -7,16 +7,22 @@ $DB_USERNAME = 'root';
 $DB_PASS = 'usbw';
 $DB_NAME = 'waiter';
 $mysqli = new mysqli($HOST, $DB_USERNAME, $DB_PASS,$DB_NAME);
-$query = $mysqli->query("DELETE FROM category WHERE name='".$_GET['catdel']."' LIMIT 1");
+$query = $mysqli->query("DELETE FROM category WHERE name=".$_GET['catdel']." LIMIT 1");
 
 //sends the query to delete the entry
 
 if ($query) {
 //if it updated
-echo $_GET['catdel'];
 ?>
 
-            <strong>Contact Has Been Deleted</strong><br /><br />
+            <strong>Contact Has Been Deleted</strong> <button onclick="sendtoindex()"> <br /><br />
+
+            <script>
+            function sendtoindex() {
+              window.location.href = 'index.php';
+            }
+            </script>
+
 
 <?php
  } else {
