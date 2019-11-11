175b8713f7809674724e2dae73e4bb89b7954107
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-11-11 12:16:44 -0300

    Add files via upload

--- inc/sidemenu.php
+++ inc/sidemenu.php
@@ -52,9 +52,9 @@ if(isset($_SESSION['slide'])){
 $categorys = $mysqli->query("SELECT * FROM category");
     while($rys = mysqli_fetch_array($categorys))
     {
-      ?>
-      <li class='tab'><a href='#menu<?php echo $rys['id'];?>'><?php echo $rys['name'];?></a></li>
-      <?php
+      
+    echo utf8_encode ("<li class='tab'><a href='#menu".$rys['id']."'>".$rys['name']."</a></li>");
+      
     }
   }
   ?>
@@ -90,7 +90,6 @@ $categorys = $mysqli->query("SELECT * FROM category");
 <?php
 if($_SESSION['admin']=='true'){
   echo "
-  <li><a class='waves-effect white-text' href='redirect.php?pagead=5&foodslide=false'><i class='material-icons white-text'>archive</i>Estoque</a></li>
   <li><a class='waves-effect white-text' href='redirect.php?profile_session=all&pagead=profile&foodslide=false'><i class='material-icons white-text'>assignment_ind</i>Funcionários</a></li>
   <li><a class='waves-effect white-text' href='redirect.php?pagead=1&foodslide=false'><i class='material-icons white-text'>content_paste</i>Pedidos</a></li>
 <li><a class='waves-effect white-text' href='redirect.php?pagead=06&foodslide=true'><i class='material-icons white-text'>archive</i>Realizar Pedidos</a></li>
@@ -120,15 +119,10 @@ if(isset($_SESSION['slide'])){
     <div id='slide-out2_ajax'>
     </div>
     <script>
-    // 1. create a new XMLHttpRequest object -- an object like any other!
     var myRequest = new XMLHttpRequest();
-    // 2. open the request and pass the HTTP method name and the resource as parameters
     myRequest.open('GET', 'inc/employers/employers_select/side_select.php');
-    // 3. write a function that runs anytime the state of the AJAX request changes
     myRequest.onreadystatechange = function () {
-    // 4. check if the request has a readyState of 4, which indicates the server has responded (complete)
     if (myRequest.readyState === 4) {
-    // 5. insert the text sent by the server into the HTML of the 'ajax-content'
     document.getElementById('slide-out2_ajax').innerHTML = myRequest.responseText;
                                   }
                                              };
@@ -145,7 +139,6 @@ if(isset($_SESSION['slide'])){
 <?php
           include 'inc/employers/insert.php';
 ?>
-
    </ul>
 <?php
 }
