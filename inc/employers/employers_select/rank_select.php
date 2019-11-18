7e8c1df06c5a878c5bc29abc9f7085a8ca7d035b
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-11-18 08:26:05 -0300

    Add files via upload

--- inc/employers/employers_select/rank_select.php
+++ inc/employers/employers_select/rank_select.php
@@ -5,9 +5,14 @@ $DB_PASS = 'usbw';
 $DB_NAME = 'waiter';
 $mysqli = new mysqli($HOST, $DB_USERNAME, $DB_PASS,$DB_NAME);
 ?>
+<div class="row">
+<div class="col s12 m5 l5">
+</div>
+<div class="col s12 m5 l5">
+
+<ol>
 
 <?php
-
 $rankcount = $mysqli->query("SELECT COUNT(*),starter_id FROM requests_numbers GROUP BY starter_id ORDER BY COUNT(*) DESC");
 while($count = mysqli_fetch_array($rankcount))
 {
@@ -15,13 +20,24 @@ $nameinrank = $mysqli->query("SELECT * FROM accounts WHERE id = '".$count['start
 while($name = mysqli_fetch_array($nameinrank))
 {
 ?>
-
-<?php  echo $name['id']; ?>
-<?php  echo $name['username']; ?>
-<?php  echo $count['COUNT(*)']; ?>
-<?php  echo "<br>"; ?>
-
+<li>
+<div class="card horizontal">
+<div class="col s12 m5 l5">
+    <div class="card-image">
+<img src="<?php  echo $name['img']; ?>" class="circle" style="width:150px; height:150px; object-fit:cover; position: relative;">
+</div>
+</div>
+     <div class="card-stacked">
+       <div class="card-content">
+<h3 style="padding-top:10%;"><?php  echo $name['username']; ?></h3>
+<strong> Pedidos realizados: </strong><?php  echo $count['COUNT(*)']; ?>
+</li><?php  echo "<br>"; ?>
   <?php
 }
   }
   ?>
+</div>
+  </div>
+</div>
+</div>
+</ol>
