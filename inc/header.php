2d3d6f8042f14787c05870df664b8b17be1ecc4e
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-11-18 20:10:21 -0300

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -15,6 +15,7 @@ $hour= date('h:i:s', time());
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="style.css" />
+    <meta charset="utf-8">
 
     <!--Let browser know website is optimized for mobile--><meta name="viewport" content="width=device-width, initial-scale=1.0"/>
    <!-- Materialize CSS --> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
