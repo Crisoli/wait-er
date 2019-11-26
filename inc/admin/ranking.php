751a4576fa4d84cce6261ac32c3639dab2395b6e
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-11-26 08:43:34 -0300

    Add files via upload

--- inc/admin/ranking.php
+++ inc/admin/ranking.php
@@ -15,7 +15,7 @@
         document.getElementById('reveal').style.display = 'none';
                           });
                           setInterval(function side_show(){
-                   $('#thegoddamnrankdiv_ajax').load('inc/employers/employers_select/rank_select.php');
+                   $('#thegoddamnrankdiv_ajax').load('inc/admin/admin_select/rank_select.php');
                 }, 2000) /* time in milliseconds (ie 2 seconds)*/
 
        </script>
