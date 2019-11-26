de7b37315e23302fb5e0961d3fd3c379f7f0981a
DudaVS <51866139+DudaVS@users.noreply.github.com>
2019-11-26 10:27:02 -0300

    foda

---
+++ accounts (1).sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 26-Nov-2019 às 13:26
+-- Versão do servidor: 5.6.34
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `waiter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phonenumber` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `usertype` int(11) NOT NULL,
+  `creator_id` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `username`, `img`, `email`, `phonenumber`, `description`, `password`, `usertype`, `creator_id`) VALUES
+(6, 'Cristian de Oliveira Menezes', 'inc/img/uploads/perfil/jose2019.11.26-11.jpg', 'h3has@viphone.eu.org', '+1-202-555-0180', 'Cozinheiro', 'cris', 11, 1),
+(7, 'Eduarda Zambom', 'inc/img/uploads/perfil/mulher2019.11.26-11.jpg', 'celestina@gmail.com', '1213141516', 'FuncionÃ¡rio Fixo', 'celestina', 11, 6),
+(8, 'Marcos', 'inc/img/uploads/perfil/test2019.11.26-11.png', 'marcos@gmail.com', '+13997085677', 'FuncionÃ¡rio Fixo', 'marcos', 10, 6),
+(14, 'Maria', 'inc/img/uploads/perfil/celestin2019.11.26-11.jpg', 'maria@gmail.com', '+13997085677', 'FuncionÃ¡rio Fixo', 'maria', 10, 6);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`,`creator_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
