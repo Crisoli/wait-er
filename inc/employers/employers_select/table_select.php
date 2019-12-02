90cea46003a1b87f26af8e10ab169d2959738582
Cristian <38947555+Crisoli@users.noreply.github.com>
2019-12-02 12:12:33 -0300

    Add files via upload

--- inc/employers/employers_select/table_select.php
+++ inc/employers/employers_select/table_select.php
@@ -4,16 +4,13 @@ $DB_USERNAME = 'root';
 $DB_PASS = 'usbw';
 $DB_NAME = 'waiter';
 $mysqli = new mysqli($HOST, $DB_USERNAME, $DB_PASS,$DB_NAME);
+session_start();
 ?>
 <div class="row">
 <div class="col s10 m10 l10 offset-l1 offset-s1">
-<select placeholder='Numero da Mesa' name='table' class="browser-default">
+<select placeholder='Numero da Mesa' readonly value="<?php echo $_SESSION['nmesa']?>" name='table' class="browser-default">
 <?php
-$tablesreq = $mysqli->query("SELECT * FROM tables where status ='Livre'");
-while ($rowtable = mysqli_fetch_array($tablesreq))
-{
-echo utf8_encode('<option value=' . $rowtable['table_number'] . '>' . $rowtable['table_number'] . ' - ' . $rowtable['size'] . ' - ' . $rowtable['status'] . '</option>');
-}
+echo '<option value=' . $_SESSION['nmesa'] . '>' . $_SESSION['nmesa'] . ' - ' . $_SESSION['tmesa'] . '</option>';
 ?>
 </select>
 </div>
